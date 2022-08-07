8c25354b98247c786490cdcd9d42e24ede2f91f1
32-Mimhakkani <mim.cse32@gmail.com>
2022-08-07 12:41:02 +0600

    global and local variable

--- global_Local.cpp
+++ global_Local.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int g = 15;  //global variable 
 int main(){
 int l=10;  // local variable 
-cout<<"Global value is ::"<<g *l<<endl;
+cout<<"Global value is ::"<<g*l<<endl;
 cout<<"Local value is ::"<<l<<endl;
 
  return 0;
