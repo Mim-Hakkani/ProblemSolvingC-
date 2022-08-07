4b8e6c5ecc22cd99cb72838a52b14bf302db3d78
32-Mimhakkani <mim.cse32@gmail.com>
2022-08-07 18:40:09 +0600

    uri problem array change is solved

--- arrayxhanges.cpp
+++ arrayxhanges.cpp
@@ -7,7 +7,7 @@ int main(){
         cin>>a[i];
 
     }
-        for(int j=19,i=0;j>=0,i<19;j--,i++)
+        for(int j=19,i=0;j>=0,i<20;j--,i++)
         {
             cout<<"N["<<i<<"] = "<<a[j]<<endl;
         }
